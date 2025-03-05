dd679b42d0a603d41a788874ee6bc1f556979373
Andrew Laws <andrewlawsmapping@gmail.com>
2025-03-05 09:55:21 -0600

    Added reading of fiels

--- panel/components/dashboard.py
+++ panel/components/dashboard.py
@@ -1,9 +1,27 @@
 # imports
-import pandas as pd
+from pathlib import Path
 
-# read in data
+import geopandas as gpd
+from param import Parameterized
+
+### read in data
+data_path = Path("data")
+
+# census data
+census_data_path = data_path / "ne_ntcna_places_census_viz.geojson"
+census_data = gpd.read_file(census_data_path)
+
+# chas data
+chas_data_path = data_path / "ne_ntcna_places_chas_viz.geojson"
+chas_data = gpd.read_file(chas_data_path)
+
 
 # subclass param.Parameterized in Dashboard class
+class NTCNA_Dashboard(Parameterized):
+    """
+    Dashboard for NTCNA data
+    """
+
 
 # define widgets from params
 
