6660fff75ca7bb1ba40d3f7273024d0a425c6bd7
Andrew Laws <andrewlawsmapping@gmail.com>
2025-03-05 10:22:56 -0600

    Updated data sources

--- panel/components/dashboard.py
+++ panel/components/dashboard.py
@@ -2,28 +2,30 @@
 from pathlib import Path
 
 import geopandas as gpd
-from param import Parameterized
+import param
 
 ### read in data
 data_path = Path("data")
 
 # census data
-census_data_path = data_path / "ne_ntcna_places_census_viz.geojson"
+census_data_path = data_path / "ne_ntcna_places_census.geojson"
 census_data = gpd.read_file(census_data_path)
 
 # chas data
-chas_data_path = data_path / "ne_ntcna_places_chas_viz.geojson"
+chas_data_path = data_path / "ne_ntcna_places_chas.geojson"
 chas_data = gpd.read_file(chas_data_path)
 
 
 # subclass param.Parameterized in Dashboard class
-class NTCNA_Dashboard(Parameterized):
+class NTCNA_Dashboard(param.Parameterized):
     """
     Dashboard for NTCNA data
     """
 
+    # define widgets from params
+    _census = param.DataFrame(census_data, precedence=0)
+    _chas = param.DataFrame(chas_data, precedence=0)
 
-# define widgets from params
 
 # setup sidebar of widgets
 
