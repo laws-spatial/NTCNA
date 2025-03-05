d011bef9fbb8bef8a9aa79d9abc21bce8b7a3807
Andrew Laws <andrewlawsmapping@gmail.com>
2025-03-05 09:20:11 -0600

    Started dashboard component

---
+++ panel/components/dashboard.py
@@ -0,0 +1,13 @@
+# imports
+
+# read in data
+
+# subclass param.Parameterized in Dashboard class
+
+# define widgets from params
+
+# setup sidebar of widgets
+
+# create median age plot
+
+# return panel object with widgets and plot
